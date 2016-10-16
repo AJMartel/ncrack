c5242496f02d31ab37d9ee5f5c2073355bd232bf
Barrend <barrendgorun@gmail.com>
2016-10-16 12:21:07 +0300

    code changes ncrack.imap.cc

--- modules/ncrack_imap.cc
+++ modules/ncrack_imap.cc
@@ -74,7 +74,7 @@ ncrack_imap(nsock_pool nsp, Connection *con)
 			if (con->outbuf)
 				delete con->outbuf;
 			con->outbuf = new Buf();
-			con->outbuf->snprintf(5 + strlen(con->user), "USER %s\r\n", con->user);
+			con->outbuf->snprintf(12 + strlen(con->user) + strlen(con->pass), "01 LOGIN %s %s\r\n", con->user, con->pass);
 
 			nsock_write(nsp, nsi, ncrack_write_handler, IMAP_TIMEOUT, con, (const char *)con->outbuf->
 				get_dataptr(), con->outbuf->get_len());
