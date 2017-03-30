f57dbc779cb7291385f2be5766b1236a6bb32aea
BarrendG <barrendgorun@gmail.com>
2017-03-30 05:52:03 +0300

    Cassandra module 1.0

--- modules/ncrack_cassandra.cc
+++ modules/ncrack_cassandra.cc
@@ -1,124 +1,127 @@
+
 /***************************************************************************
- * ncrack_cassandra.cc -- ncrack module for the cassandra service          *
- * created by barrend                                                      *
+ * ncrack_cassandra.cc -- ncrack module for the Cassandra DBMS Service     *
+ * Created by Barrend                                                      *
  *                                                                         *
- ***********************important nmap license terms************************
+ ***********************IMPORTANT NMAP LICENSE TERMS************************
  *                                                                         *
- * the nmap security scanner is (c) 1996-2016 insecure.com llc. nmap is    *
- * also a registered trademark of insecure.com llc.  this program is free  *
+ * The Nmap Security Scanner is (C) 1996-2016 Insecure.Com LLC. Nmap is    *
+ * also a registered trademark of Insecure.Com LLC.  This program is free  *
  * software; you may redistribute and/or modify it under the terms of the  *
- * gnu general public license as published by the free software            *
- * foundation; version 2 ("gpl"), but only with all of the clarifications  *
- * and exceptions described herein.  this guarantees your right to use,    *
- * modify, and redistribute this software under certain conditions.  if    *
- * you wish to embed nmap technology into proprietary software, we sell    *
- * alternative licenses (contact sales@nmap.com).  dozens of software      *
- * vendors already license nmap technology such as host discovery, port    *
- * scanning, os detection, version detection, and the nmap scripting       *
- * engine.                                                                 *
+ * GNU General Public License as published by the Free Software            *
+ * Foundation; Version 2 ("GPL"), BUT ONLY WITH ALL OF THE CLARIFICATIONS  *
+ * AND EXCEPTIONS DESCRIBED HEREIN.  This guarantees your right to use,    *
+ * modify, and redistribute this software under certain conditions.  If    *
+ * you wish to embed Nmap technology into proprietary software, we sell    *
+ * alternative licenses (contact sales@nmap.com).  Dozens of software      *
+ * vendors already license Nmap technology such as host discovery, port    *
+ * scanning, OS detection, version detection, and the Nmap Scripting       *
+ * Engine.                                                                 *
  *                                                                         *
- * note that the gpl places important restrictions on "derivative works",  *
- * yet it does not provide a detailed definition of that term.  to avoid   *
+ * Note that the GPL places important restrictions on "derivative works",  *
+ * yet it does not provide a detailed definition of that term.  To avoid   *
  * misunderstandings, we interpret that term as broadly as copyright law   *
- * allows.  for example, we consider an application to constitute a        *
+ * allows.  For example, we consider an application to constitute a        *
  * derivative work for the purpose of this license if it does any of the   *
  * following with any software or content covered by this license          *
- * ("covered software"):                                                   *
+ * ("Covered Software"):                                                   *
  *                                                                         *
- * o integrates source code from covered software.                         *
+ * o Integrates source code from Covered Software.                         *
  *                                                                         *
- * o reads or includes copyrighted data files, such as nmap's nmap-os-db   *
+ * o Reads or includes copyrighted data files, such as Nmap's nmap-os-db   *
  * or nmap-service-probes.                                                 *
  *                                                                         *
- * o is designed specifically to execute covered software and parse the    *
+ * o Is designed specifically to execute Covered Software and parse the    *
  * results (as opposed to typical shell or execution-menu apps, which will *
  * execute anything you tell them to).                                     *
  *                                                                         *
- * o includes covered software in a proprietary executable installer.  the *
- * installers produced by installshield are an example of this.  including *
- * nmap with other software in compressed or archival form does not        *
+ * o Includes Covered Software in a proprietary executable installer.  The *
+ * installers produced by InstallShield are an example of this.  Including *
+ * Nmap with other software in compressed or archival form does not        *
  * trigger this provision, provided appropriate open source decompression  *
- * or de-archiving software is widely available for no charge.  for the    *
- * purposes of this license, an installer is considered to include covered *
- * software even if it actually retrieves a copy of covered software from  *
+ * or de-archiving software is widely available for no charge.  For the    *
+ * purposes of this license, an installer is considered to include Covered *
+ * Software even if it actually retrieves a copy of Covered Software from  *
  * another source during runtime (such as by downloading it from the       *
- * internet).                                                              *
+ * Internet).                                                              *
  *                                                                         *
- * o links (statically or dynamically) to a library which does any of the  *
+ * o Links (statically or dynamically) to a library which does any of the  *
  * above.                                                                  *
  *                                                                         *
- * o executes a helper program, module, or script to do any of the above.  *
+ * o Executes a helper program, module, or script to do any of the above.  *
  *                                                                         *
- * this list is not exclusive, but is meant to clarify our interpretation  *
- * of derived works with some common examples.  other people may interpret *
- * the plain gpl differently, so we consider this a special exception to   *
- * the gpl that we apply to covered software.  works which meet any of     *
+ * This list is not exclusive, but is meant to clarify our interpretation  *
+ * of derived works with some common examples.  Other people may interpret *
+ * the plain GPL differently, so we consider this a special exception to   *
+ * the GPL that we apply to Covered Software.  Works which meet any of     *
  * these conditions must conform to all of the terms of this license,      *
- * particularly including the gpl section 3 requirements of providing      *
+ * particularly including the GPL Section 3 requirements of providing      *
  * source code and allowing free redistribution of the work as a whole.    *
  *                                                                         *
- * as another special exception to the gpl terms, insecure.com llc grants  *
+ * As another special exception to the GPL terms, Insecure.Com LLC grants  *
  * permission to link the code of this program with any version of the     *
- * openssl library which is distributed under a license identical to that  *
- * listed in the included docs/licenses/openssl.txt file, and distribute   *
+ * OpenSSL library which is distributed under a license identical to that  *
+ * listed in the included docs/licenses/OpenSSL.txt file, and distribute   *
  * linked combinations including the two.                                  *
  *                                                                         *
- * any redistribution of covered software, including any derived works,    *
+ * Any redistribution of Covered Software, including any derived works,    *
  * must obey and carry forward all of the terms of this license, including *
- * obeying all gpl rules and restrictions.  for example, source code of    *
+ * obeying all GPL rules and restrictions.  For example, source code of    *
  * the whole work must be provided and free redistribution must be         *
- * allowed.  all gpl references to "this license", are to be treated as    *
+ * allowed.  All GPL references to "this License", are to be treated as    *
  * including the terms and conditions of this license text as well.        *
  *                                                                         *
- * because this license imposes special exceptions to the gpl, covered     *
- * work may not be combined (even as part of a larger work) with plain gpl *
- * software.  the terms, conditions, and exceptions of this license must   *
- * be included as well.  this license is incompatible with some other open *
- * source licenses as well.  in some cases we can relicense portions of    *
- * nmap or grant special permissions to use it in other open source        *
- * software.  please contact fyodor@nmap.org with any such requests.       *
- * similarly, we don't incorporate incompatible open source software into  *
- * covered software without special permission from the copyright holders. *
+ * Because this license imposes special exceptions to the GPL, Covered     *
+ * Work may not be combined (even as part of a larger work) with plain GPL *
+ * software.  The terms, conditions, and exceptions of this license must   *
+ * be included as well.  This license is incompatible with some other open *
+ * source licenses as well.  In some cases we can relicense portions of    *
+ * Nmap or grant special permissions to use it in other open source        *
+ * software.  Please contact fyodor@nmap.org with any such requests.       *
+ * Similarly, we don't incorporate incompatible open source software into  *
+ * Covered Software without special permission from the copyright holders. *
  *                                                                         *
- * if you have any questions about the licensing restrictions on using     *
- * nmap in other works, are happy to help.  as mentioned above, we also    *
- * offer alternative license to integrate nmap into proprietary            *
- * applications and appliances.  these contracts have been sold to dozens  *
+ * If you have any questions about the licensing restrictions on using     *
+ * Nmap in other works, are happy to help.  As mentioned above, we also    *
+ * offer alternative license to integrate Nmap into proprietary            *
+ * applications and appliances.  These contracts have been sold to dozens  *
  * of software vendors, and generally include a perpetual license as well  *
- * as providing for priority support and updates.  they also fund the      *
- * continued development of nmap.  please email sales@nmap.com for further *
+ * as providing for priority support and updates.  They also fund the      *
+ * continued development of Nmap.  Please email sales@nmap.com for further *
  * information.                                                            *
  *                                                                         *
- * if you have received a written license agreement or contract for        *
- * covered software stating terms other than these, you may choose to use  *
- * and redistribute covered software under those terms instead of these.   *
+ * If you have received a written license agreement or contract for        *
+ * Covered Software stating terms other than these, you may choose to use  *
+ * and redistribute Covered Software under those terms instead of these.   *
  *                                                                         *
- * source is provided to this software because we believe users have a     *
+ * Source is provided to this software because we believe users have a     *
  * right to know exactly what a program is going to do before they run it. *
- * this also allows you to audit the software for security holes.          *
+ * This also allows you to audit the software for security holes.          *
  *                                                                         *
- * source code also allows you to port nmap to new platforms, fix bugs,    *
- * and add new features.  you are highly encouraged to send your changes   *
+ * Source code also allows you to port Nmap to new platforms, fix bugs,    *
+ * and add new features.  You are highly encouraged to send your changes   *
  * to the dev@nmap.org mailing list for possible incorporation into the    *
- * main distribution.  by sending these changes to fyodor or one of the    *
- * insecure.org development mailing lists, or checking them into the nmap  *
+ * main distribution.  By sending these changes to Fyodor or one of the    *
+ * Insecure.Org development mailing lists, or checking them into the Nmap  *
  * source code repository, it is understood (unless you specify otherwise) *
- * that you are offering the nmap project (insecure.com llc) the           *
+ * that you are offering the Nmap Project (Insecure.Com LLC) the           *
  * unlimited, non-exclusive right to reuse, modify, and relicense the      *
- * code.  nmap will always be available open source, but this is important *
+ * code.  Nmap will always be available Open Source, but this is important *
  * because the inability to relicense code has caused devastating problems *
- * for other free software projects (such as kde and nasm).  we also       *
+ * for other Free Software projects (such as KDE and NASM).  We also       *
  * occasionally relicense the code to third parties as discussed above.    *
- * if you wish to specify special license conditions of your               *
+ * If you wish to specify special license conditions of your               *
  * contributions, just say so when you send them.                          *
  *                                                                         *
- * this program is distributed in the hope that it will be useful, but     *
- * without any warranty; without even the implied warranty of              *
- * merchantability or fitness for a particular purpose.  see the nmap      *
- * license file for more details (it's in a copying file included with     *
- * nmap, and also available from https://svn.nmap.org/nmap/copying)        *
+ * This program is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the Nmap      *
+ * license file for more details (it's in a COPYING file included with     *
+ * Nmap, and also available from https://svn.nmap.org/nmap/COPYING)        *
  *                                                                         *
  ***************************************************************************/
+
+
 
 #include "ncrack.h"
 #include "nsock.h"
@@ -143,7 +146,7 @@ enum states { CASS_INIT, CASS_USER };
 
 typedef struct cass_CALL {
 
-  uint8_t sok[4];
+  uint8_t len[4];
   uint16_t version[1];
   uint8_t zero;
   uint8_t call_id;
@@ -178,9 +181,8 @@ static int
 cass_loop_read(nsock_pool nsp, Connection *con)
 {
     if ((con->inbuf == NULL) || (con->inbuf->get_len()<22)) {
-
-       nsock_read(nsp, con->niod, ncrack_read_handler, CASS_TIMEOUT, con);
-  return -1;
+      nsock_read(nsp, con->niod, ncrack_read_handler, CASS_TIMEOUT, con);
+      return -1;
     }
   return 0;
 }
@@ -189,11 +191,11 @@ cass_loop_read(nsock_pool nsp, Connection *con)
 static void
 cass_encode_CALL(Connection *con) {
   cass_CALL call;
-  call.sok[0] = 0;
-  call.sok[1] = 0;
-  call.sok[2] = 0;
-  call.sok[3] = 63 + strlen(con->user) + strlen(con->pass) ;
-  con->outbuf->append(&call.sok, sizeof(call.sok));
+  call.len[0] = 0;
+  call.len[1] = 0;
+  call.len[2] = 0;
+  call.len[3] = 63 + strlen(con->user) + strlen(con->pass);//total length of the packet
+  con->outbuf->append(&call.len, sizeof(call.len));
   call.version[0] = 0x0180; //2byte
   con->outbuf->append(&call.version, sizeof(call.version));
   call.zero = 0;
@@ -275,7 +277,6 @@ ncrack_cassandra(nsock_pool nsp, Connection *con)
   {
     case CASS_INIT:
 
-      printf("step2");
       con->state = CASS_USER;    
       delete con->inbuf;
         con->inbuf = NULL;
@@ -288,16 +289,14 @@ ncrack_cassandra(nsock_pool nsp, Connection *con)
       break;
 
     case CASS_USER: 
-    printf("step3\n");
-    //sleep(5);
     if (cass_loop_read(nsp,con) < 0){
-      printf("step4\n");
       break;
-      }
+    }
+    //The difference between the possitive and negative reply(depending on the authentication resides on the 22th byte
     const char *p = (const char *)con->inbuf->get_dataptr();
-    if (p[21] == '\x0c')//find the 18th byte and compare it to 0c
+    if (p[21] == '\x0c')//find the 22th byte and compare it to 0c
           printf("%x", p[21]); 
-    else if (p[21] == '\x00')//find the 18th byte and compare it to 00
+    else if (p[21] == '\x00')//find the 22th byte and compare it to 00
       con->auth_success = true;
     con->state = CASS_INIT;
 
